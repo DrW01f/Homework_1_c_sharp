b83454801e5a0d0e970dee338213e63c190b7af7
Andrey <Dr_Wo1f@mail.ru>
2023-01-26 21:28:31 +0300

    Task 1 upgraded

--- Program.cs
+++ Program.cs
@@ -1,5 +1,6 @@
 ﻿// Задача 2: Напишите программу, которая на вход принимает два числа и выдаёт, 
 //какое число большее, а какое меньшее.
+
 Console.WriteLine ("Введите первое число");
 int first_count = int.Parse(Console.ReadLine()!);
 Console.WriteLine ("Введите второе число");
@@ -7,9 +8,15 @@ int second_count = int.Parse(Console.ReadLine()!);
 Console.WriteLine ("Немного магии...");
 if (first_count > second_count)
 {
-    Console.WriteLine ($"Больше число {first_count} ");
+    Console.WriteLine ($"Большее число {first_count} ");
+    Console.WriteLine ($"Меньшее число {second_count} ");
 }
 else
 {
-    Console.WriteLine ($"Больше число {second_count} ");
+    Console.WriteLine ($"Большее число {second_count} ");
+    Console.WriteLine ($"Меньшее число {first_count} ");
 }
+
+//Задача 4: Напишите программу, которая принимает на вход три числа и выдаёт 
+//максимальное из этих чисел.
+
