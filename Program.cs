8aafa0ffe587663e45d34ac290f0d9034a4db9be
Andrey <Dr_Wo1f@mail.ru>
2023-01-26 22:38:03 +0300

    Task 2 complete, task 1 fixed

--- Program.cs
+++ Program.cs
@@ -1,22 +1,42 @@
 ﻿// Задача 2: Напишите программу, которая на вход принимает два числа и выдаёт, 
 //какое число большее, а какое меньшее.
 
-Console.WriteLine ("Введите первое число");
-int first_count = int.Parse(Console.ReadLine()!);
-Console.WriteLine ("Введите второе число");
-int second_count = int.Parse(Console.ReadLine()!);
-Console.WriteLine ("Немного магии...");
-if (first_count > second_count)
-{
-    Console.WriteLine ($"Большее число {first_count} ");
-    Console.WriteLine ($"Меньшее число {second_count} ");
-}
-else
-{
-    Console.WriteLine ($"Большее число {second_count} ");
-    Console.WriteLine ($"Меньшее число {first_count} ");
-}
+// Console.WriteLine ("Введите первое число");
+// int first_count = int.Parse(Console.ReadLine()!);
+// Console.WriteLine ("Введите второе число");
+// int second_count = int.Parse(Console.ReadLine()!);
+// Console.WriteLine ("Немного магии...");
+
+// if (first_count > second_count)
+// {
+//     Console.WriteLine ($"max = {first_count} ");
+// }
+
+// else
+// {
+//     Console.WriteLine ($"max = {second_count} ");
+
+// }
 
 //Задача 4: Напишите программу, которая принимает на вход три числа и выдаёт 
 //максимальное из этих чисел.
 
+// Console.WriteLine ("Введите первое число");
+// int first_count = int.Parse(Console.ReadLine()!);
+// Console.WriteLine ("Введите второе число");
+// int second_count = int.Parse(Console.ReadLine()!);
+// Console.WriteLine ("Введите третье число");
+// int third_count = int.Parse(Console.ReadLine()!);
+// int max = first_count;
+
+// if (second_count > max)
+// {
+//     max = second_count;
+// }
+
+// if (third_count > max)
+// {
+//     max = third_count;
+// }
+
+// Console.WriteLine (max);
