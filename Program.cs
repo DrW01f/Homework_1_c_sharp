4fe2229d9cc64b6565496b2edc89d0b4e36baa86
Andrey <Dr_Wo1f@mail.ru>
2023-01-26 22:43:58 +0300

    Task 3 complete

--- Program.cs
+++ Program.cs
@@ -40,3 +40,19 @@
 // }
 
 // Console.WriteLine (max);
+
+//Задача 6: Напишите программу, которая на вход принимает число и выдаёт, 
+//является ли число чётным (делится ли оно на два без остатка).
+
+Console.WriteLine ("Введите число");
+int count = int.Parse(Console.ReadLine()!);
+
+if (count % 2 == 0)
+{
+    Console.WriteLine ("да");
+}
+
+else 
+{
+    Console.WriteLine ("нет");
+}
